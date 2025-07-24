c7a501f1fbc1ce6d5732d0f77ec7b076426c4d8b
Esteham H. Zihad ansari <eshasan1287005@gmail.com>
2025-07-24 15:59:55 +0600

    create a login page for log in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import './index.css';
 import './assets/css/header.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Login from './components/Login'
 
 function App() {
   return (
@@ -11,6 +15,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
